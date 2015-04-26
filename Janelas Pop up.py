ba8115b44a7414148d5b418d9064789f1918c3b8
Rodrigo Ronchail Gikas <rodrigo.rgk@hotmail.com>
2015-04-26 15:24:53 -0300

    Novos comandos

---
+++ Janelas Pop up.py
@@ -0,0 +1,6 @@
+import ctypes
+
+ctypes.windll.user32.MessageBoxW(None,'Como vai?','ola',0)
+ctypes.windll.user32.MessageBoxW(None,'Como vai?','ola',1)
+ctypes.windll.user32.MessageBoxW(None,'Como vai?','ola',2)
+ctypes.windll.user32.MessageBoxW(None,'Como vai?','ola',3)
